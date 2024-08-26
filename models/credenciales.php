0e2766e1ae08d04831921f532ccb43b41ca0a85a
facu <facundosandoval376@gmail.com>
2024-08-26 20:37:47 -0300

    conexion a la DB

---
+++ models/credenciales.php
@@ -0,0 +1,7 @@
+ <?php
+    define('HOST', '7dt.h.filess.io');
+    define('USER', 'sayc_summercan');
+    define('PASS', 'f63f43dc3c2f50435ac7638e968aa43aa0bca1a9');
+    define('DB', 'sayc_summercan');
+    define('PORT', 3307);
+ ?>
