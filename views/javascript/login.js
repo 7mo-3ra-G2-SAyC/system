08d3feb09b44e526b8f88da442cf2122dcfc4f70
tbehm <166077096+tbehm@users.noreply.github.com>
2024-09-02 20:49:45 -0300

    Update login.js

--- views/javascript/login.js
+++ views/javascript/login.js
@@ -1,3 +1,9 @@
+
+const fetchApi = async(model, method, params)=>{
+    var response = await fetch(`https://sayc-76fee51f670d.herokuapp.com/api/${model}/${method}/?${params}`);
+    response = await response.json();
+    return response;
+}
 const a = await fetchApi('cronograma','getByActivityId','id=1');
 console.log(a);
 console.log("hola")
