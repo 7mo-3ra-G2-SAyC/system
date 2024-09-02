db3c814278b712276b5626b9b613caaf04a44bf9
tbehm <166077096+tbehm@users.noreply.github.com>
2024-09-02 20:48:27 -0300

    Update loginController.php

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,13 +2,6 @@
 
 	// inicializa el obejto y carga la vista
 	$tpl = new TPLEngine('login');
-
-	$head = new TPLEngine("header");
-	// carga la vista
-    $vars['HEADER'] = $head->getVista();
-   	
-    // reemplaza variables en la vista
-    $tpl->setVars($vars);
 	// imprime la vista
 	$tpl->print();
 
