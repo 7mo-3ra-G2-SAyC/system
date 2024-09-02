18fb7713ec7f78f4e34fc09af2814dcbbf3fa006
Facundo Sandoval <163065420+sandooo24@users.noreply.github.com>
2024-09-02 20:33:35 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@
 	$request_method = $_SERVER["REQUEST_METHOD"];
 
 	// Captura lo que sigue luego de /api/ en la URL.
-	$urlResult = str_replace('sayc-76fee51f670d.herokuapp.com/api/', '', $_SERVER["REQUEST_URI"]);
+	$urlResult = str_replace('sayc-76fee51f670d.herokuapp.com/api/index.php', '', $_SERVER["REQUEST_URI"]);
 
 	// Se separa el contenido de $urlResult en 2 posiciones (modelo/método).
 	$urlExplode = explode('/', $urlResult);
