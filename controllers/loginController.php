3db2ea86903445b6b9f74bb8508b900953e1f9b6
facu <facundosandoval376@gmail.com>
2024-09-02 21:32:16 -0300

    login 2

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,6 +2,18 @@
 
 	// inicializa el obejto y carga la vista
 	$tpl = new TPLEngine('login');
+
+	// carga la vista del header
+	$header = new TPLEngine('header');
+
+	// carga la variables de reempleazo
+	$vars=[
+		'HEADER' => $header->getVista()
+	];
+
+	// reemlpaza las variables de la vista
+	$tpl->setVars($vars);
+	
 	// imprime la vista
 	$tpl->print();
 
