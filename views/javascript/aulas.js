d11f879aa575d1b4cae9c552a8d6440b826ca4f2
tbehm <166077096+tbehm@users.noreply.github.com>
2024-09-22 19:13:22 -0300

    Update aulas.js

--- views/javascript/aulas.js
+++ views/javascript/aulas.js
@@ -26,27 +26,27 @@
 	 	 * @brief Agrega una fila dentro de la tabla
 	 	 * @param object data fila con datos de la actividad 
 	 	 * */
-		function addRowListado(data){
+		// function addRowListado(data){
 
-			/*< captura el contenido de la template */
-			const tpl = tpl_row.content;
-			/*< clona la plantilla como un nodo completo*/
-			const clon = tpl.cloneNode(true);
-			/*< alteramos el contenido del clon con los valores de data */
-			clon.querySelector(".activity").innerHTML = data.activity_name;
-			clon.querySelector(".activity_category").innerHTML = data.activity_category;
-			clon.querySelector(".hora").innerHTML = inicio;
-			clon.querySelector(".duracion").innerHTML = data.duracion;
-			let date = new Date();
-			if(data.inicio < date){
-				let func = data.inicio+data.duracion;
-				if(func >= date){
-					clon.style.background = "orange";
-				}
-				else{
-					clon.style.background = "red";
-				}
-			}
-			/*< inserta el clon modificado dentro de la tabla */
-			cronograma.appendChild(clon);
-		}
+		// 	/*< captura el contenido de la template */
+		// 	const tpl = tpl_row.content;
+		// 	/*< clona la plantilla como un nodo completo*/
+		// 	const clon = tpl.cloneNode(true);
+		// 	/*< alteramos el contenido del clon con los valores de data */
+		// 	clon.querySelector(".activity").innerHTML = data.activity_name;
+		// 	clon.querySelector(".activity_category").innerHTML = data.activity_category;
+		// 	clon.querySelector(".hora").innerHTML = inicio;
+		// 	clon.querySelector(".duracion").innerHTML = data.duracion;
+		// 	let date = new Date();
+		// 	if(data.inicio < date){
+		// 		let func = data.inicio+data.duracion;
+		// 		if(func >= date){
+		// 			clon.style.background = "orange";
+		// 		}
+		// 		else{
+		// 			clon.style.background = "red";
+		// 		}
+		// 	}
+		// 	/*< inserta el clon modificado dentro de la tabla */
+		// 	cronograma.appendChild(clon);
+		// }
