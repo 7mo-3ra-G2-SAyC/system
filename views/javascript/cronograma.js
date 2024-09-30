c3cad84633f49c48b919726d467c45126dacbd7c
Rusiton <galassosantiago05@gmail.com>
2024-09-30 18:21:53 -0300

    Changed files

--- views/javascript/cronograma.js
+++ views/javascript/cronograma.js
@@ -4,6 +4,8 @@ async function setTableContent(){
 
     const content = await fetchApi("cronograma", "getFullSchedule");
 
+    console.log(content);
+
     content.forEach( row => {
 
         let tr = document.createElement("tr");
--- views/javascript/cronogramaGuias.js
+++ views/javascript/cronogramaGuias.js
@@ -1,4 +1,4 @@
-const cronTable = document.querySelector("#table-cronograma tbody");
+const cronTable = document.querySelector(".table-cronograma.actual tbody");
 
 async function setTableContent(){
 
